0a65a84847eb0ecef88718a7c01aadff80ecbafc
Pham Minh Phi <phamminhphi2903@gmail.com>
2026-03-30 08:39:03 +0700

    update message & log

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 # SangT2-30-03
+=======
+# NNPTUD-S2
 Lớp NNPTUD-Sáng t2
 Mỗi ngày 1 nhánh
+>>>>>>> 1cd9713 ( CRUD by fetch)
---
+++ schemas/messages.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const messageSchema = new mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      required: true
+    },
+    contentMessage: {
+      type: {
+        type: String,
+        enum: ["text", "file"],
+        required: true
+      },
+      content: {
+        type: String,
+        required: true
+      }
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("message", messageSchema);
